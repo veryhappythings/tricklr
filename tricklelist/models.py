a90b8bd75ace0705008b1f5fa6d3942cfa585ace
mac <mac.s.c.chapman@gmail.com>
2010-07-02 19:52:32 +0100

    Models now allow you to create a list and do an item in it

--- tricklelist/models.py
+++ tricklelist/models.py
@@ -1,3 +1,28 @@
+import datetime
 from django.db import models
 
-# Create your models here.
+# TrickleList has many items
+# items are done by a user, on a day
+
+class TrickleList(models.Model):
+    name = models.CharField(max_length=100)
+
+class ListItem(models.Model):
+    trickle_list = models.ForeignKey(TrickleList)
+    name = models.CharField(max_length=50)
+
+    def complete(self, date=None):
+        if date == None:
+            date = datetime.datetime.utcnow()
+        done = self.doneitem_set.create(date=date)
+        done.save()
+
+    def is_done(self, date=None):
+        if date == None:
+            date = datetime.datetime.utcnow()
+        return len(self.doneitem_set.select_related().dates('date', 'day')) > 0
+
+class DoneItem(models.Model):
+    list_item = models.ForeignKey(ListItem)
+    date = models.DateField()
+
--- tricklelist/tests.py
+++ tricklelist/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 from django.test.client import Client
 
-class TestTrickleList(TestCase):
+from models import TrickleList, ListItem, DoneItem
+
+class TestTrickleListApp(TestCase):
     def setup(self):
         self.client = Client()
 
@@ -12,4 +14,33 @@ class TestTrickleList(TestCase):
     def test_that_index_page_exists(self):
         response = self.client.get('/trickle/')
         self.assertEqual(response.status_code, 200)
+
+class TestTrickleList(TestCase):
+    def test_adding_list_items(self):
+        tl = TrickleList(name='Test List')
+        tl.save()
+        li = ListItem(name='Test Item', trickle_list=tl)
+        li.save()
+        self.assertEqual(li.trickle_list.id, tl.id)
+
+        li = tl.listitem_set.create(name='Another test item')
+        li.save()
+        self.assertEqual(li.trickle_list.id, tl.id)
+
+        self.assertEqual(
+            map(lambda i: i.name, tl.listitem_set.all()),
+            [u'Test Item', u'Another test item']
+        )
+
+class TestListItem(TestCase):
+    def test_completing_an_item(self):
+        tl = TrickleList(name='Test List')
+        tl.save()
+        li = tl.listitem_set.create(name='Test item')
+        li.complete()
+        li.save()
+        self.assert_(li.is_done())
+
+class TestDoneItem(TestCase):
+    pass
 
