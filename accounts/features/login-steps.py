fe2602ce5233d488a9f625634b7ea579564a7a2d
mac <mac@veryhappythings.co.uk>
2011-01-19 07:53:38 +0000

    Adds Selenium to Lettuce

--- accounts/features/login-steps.py
+++ accounts/features/login-steps.py
@@ -14,18 +14,18 @@ def given_that_i_have_an_account(step, username, password):
 
 @step(u'When I go to the log in page')
 def when_i_go_to_the_log_in_page(step):
-    world.browser.get(django_url('/accounts/login'))
+    world.browser.open(django_url('/accounts/login'))
 
-@step(u'And I login with valid credentials')
-def and_i_login_with_valid_credentials(step):
-    world.response = world.browser.post(
-        django_url('/accounts/login'),
-        {'username': 'test_user1', 'password': 'test_password', 'next': '/accounts/profile'}
-    )
+@step(u'And I login with username "(.*)" and password "(.*)"')
+def and_i_login_with_valid_credentials(step, username, password):
+    world.browser.type('username', username)
+    world.browser.type('password', password)
+    world.browser.click('submit')
+    world.browser.wait_for_page_to_load(30000)
 
 @step(u'Then I should see "(.*)"')
 def then_i_should_see_(step, phrase):
-    assert re.search(phrase, world.response.content)
+    assert world.browser.is_text_present(phrase)
 
 @step(u'Then I should be redirected')
 def then_i_should_be_redirected(step):
--- terrain.py
+++ terrain.py
@@ -1,7 +1,9 @@
-from lettuce import world, before
+from lettuce import world, before, after
+from lettuce.django import django_url
 from django.test.client import Client
 from django.test.utils import setup_test_environment
 from django.core.management import call_command
+from selenium import selenium
 
 world.browser = Client()
 
@@ -11,4 +13,16 @@ def initial_setup(server):
     call_command('flush', interactive=False)
     call_command('loaddata', 'all')
     setup_test_environment()
+
+@before.harvest
+def prepare_browser_driver(variables):
+    if variables.get('run_server', False) is True:
+        print 'Starting Selenium...'
+        world.browser = selenium('localhost', 4444, '*firefox', django_url('/'))
+        world.browser.start()
+        print 'Selenium started.'
+
+@after.harvest
+def shutdown_browser_driver(results):
+    world.browser.stop()
 
