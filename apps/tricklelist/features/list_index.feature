fa096411372978d435133d75db6a0fd6953fb738
mac <mac@veryhappythings.co.uk>
2011-01-24 08:16:56 +0000

    WIP on list index functionality

--- apps/tricklelist/features/list_index.feature
+++ apps/tricklelist/features/list_index.feature
@@ -8,3 +8,10 @@ Feature: List index
     And I have 1 list
     And I go to the list index
     Then I should see 1 list
+
+  Scenario: Select list
+    Given that I am logged in
+    And I have 1 list
+    And I go to the list index
+    And I click on the first list
+    Then I should see that list on the list display page
--- apps/tricklelist/features/step_definitions/list_index_steps.py
+++ apps/tricklelist/features/step_definitions/list_index_steps.py
@@ -3,17 +3,25 @@ from lettuce import step, world
 from lettuce.django import django_url
 from nose.tools import assert_equal
 
-
-
+# Ands
 @step(u'And I go to the list index')
 def and_i_go_to_the_list_index(step):
     world.browser.open(django_url('/trickle/'))
     world.browser.wait_for_page_to_load(30000)
 
+@step(u'And I click on the first list')
+def and_i_click_on_the_first_list(step):
+    world.browser.click('css=li.tricklelist')
+    world.browser.wait_for_page_to_load(30000)
+
+# Thens
 @step(u'Then I should see (\d+) lists?')
 def then_i_should_see_n_lists(step, quantity):
     quantity = int(quantity)
     count = int(world.browser.get_xpath_count('//li[@class="tricklelist"]'))
     assert_equal(count, quantity)
 
+@step(u'Then I should see that list on the list display page')
+def then_i_should_see_that_list_on_the_list_display_page(step):
+    assert False, 'This step must be implemented'
 
